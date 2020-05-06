c3574290638f6f16d0882b1c76fc101e3e617bd1
silencejamie <as124122323@gmail.com>
2020-05-06 12:32:27 +0800

    200506 push

---
+++ SelfLearn/總複習/200425_Django/200501_ORM深度查詢/mysite/mysite/urls.py
@@ -0,0 +1,9 @@
+from django.conf.urls import url
+from django.contrib import admin
+from app01 import views
+
+urlpatterns = [
+    url(r'^admin/', admin.site.urls),
+    url(r'^index/', views.index),
+    url(r'^search_book/', views.search_book),
+]
